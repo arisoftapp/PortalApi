587f783053f74125341910f59aa53e875d685559
Edgar Rembao <edgar@arisoft.com.mx>
2019-07-30 13:04:39 -0600

    ajuste de api

--- src/routes/citas.js
+++ src/routes/citas.js
@@ -18,13 +18,47 @@ app.get('/prioridad', (req, res) => {
     });
 });
 
+app.get('/citas', (req, res) => {
+    cita.getCitas((err, data) => {
+        if (err) {
+            res.json({
+                success: false,
+                message: "Ocurrió un error al obtener los datos"
+            });
+        } else{
+            res.json({
+                success: true,
+                data: data
+            });
+        }
+    });
+});
+
+app.get('/tipo', (req, res) => {
+    cita.getTipo((err, data) => {
+        if (err) {
+            res.json({
+                success: false,
+                message: "Ocurrió un error al obtener los datos"
+            });
+        } else{
+            res.json({
+                success: true,
+                data: data
+            });
+        }
+    });
+});
+
 app.post('/citas', (req, res) => {
     const citaData = {
         id_cita : req.body.id_cita,
         id_paciente: req.body.id_paciente,
         id_medico:  req.body.id_medico,
         id_prioridad:   req.body.id_prioridad,
-        fecha:  req.body.fecha_ini,
+        id_tipo:   req.body.id_tipo,
+        hora:   req.body.hora,
+        fecha:  req.body.fecha,
         sintomas:   req.body.sintomas,
         status: req.body.status
     };
